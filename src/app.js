a41010e2efe2a04cd7081cca9ada9d61a6cbb65b
Raelr <aryehzinn@hotmail.com>
2020-02-19 16:45:44 +1100

    Added yargs for all three character stats. Need to add error handling

--- src/app.js
+++ src/app.js
@@ -2,18 +2,28 @@ const yargs = require('yargs')
 const char = require('./util/char')
 
 yargs.command({
-  command: 'new_char',
-  describe: 'Creates a new character',
-  builder: {
+    command: 'new_char',
+    describe: 'Creates a new character',
+    builder: {
     name: {
-      describe: 'Character Name',
-      demandOption: true,
-      type: 'string'
+        describe: 'Character Name',
+        demandOption: true,
+        type: 'string'
+        }
+    },
+    concept: {
+        describe: 'Character concept',
+        demandOption: false,
+        type: 'string'
+    },
+    stock: {
+        describe: 'What stock do you belong to? [Human/Dwarf/Elf/Orc]',
+        demandOption: true,
+        type: 'string'
+    }, 
+    handler(argv) {
+    char.addChar(argv.name, argv.concept, argv.stock)
     }
-  },
-  handler(argv) {
-    char.addChar(argv.name)
-  }
 })
 
 yargs.parse();
--- src/util/char.js
+++ src/util/char.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 
-const addChar = (name) => {
-
-  const data = JSON.stringify({ name: name, concept: '', stock: '' }, null, 2)
-  fs.writeFileSync(name + '.json', data)
-  console.log(name + ' added!')
+const addChar = (name, concept, stock) => {
+    
+    if (stock.toLowerCase() == 'dwarf' || stock.toLowerCase() == 'human' 
+    || stock.toLowerCase() == 'orc' || stock.toLowerCase() == 'elf') {
+        const data = JSON.stringify({ name: name, concept: concept, stock: stock }, null, 2)
+        fs.writeFileSync('./characters/' + name + '.json', data)
+        console.log(name + ' added!')
+    }
 }
 
 module.exports = {
-  addChar: addChar
+    addChar: addChar
 };
