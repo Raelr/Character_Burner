183af1e20c08454691d2bd6f79c8d933540aea05
Aryeh <aryehzinn@hotmail.com>
2020-02-25 08:30:17 +1100

    Organised lifepaths to fall into categories. Need to connect new organisation to character creator

--- src/util/lifepath.js
+++ src/util/lifepath.js
@@ -12,34 +12,66 @@ const addLifePath = (name, setting, stock, time, leads, skills, traits) => {
 
     var paths = loadLifePaths()
 
-    //var hasSetting = (paths.filter((setting) => setting.name.toLowerCase() === setting.toLowerCase()).length > 0)
-
     // TODO: Check if setting actually exists.
     // TODO: If Setting does not exist - create new setting field and insert it with the correct name.
     // TODO: If setting does exist - simply insert new lifepath into the setting array.
     // TODO: Reconfigure all dependecies on the new structure.
-    //if (hasSetting) {
 
-    //}
-
-    if (!(paths.filter((lp) => lp.name === name && lp.setting === setting).find(
-        (lp) => lp.name === name && lp.setting === setting))) {
-
-        var data = {
-            name: name,
-            setting: setting,
-            stock: stock,
-            time: time,
-            leads: leads,
-            skills: skills,
-            traits: traits
-        }
-        paths.push(data)
-        saveLifePath(paths)
-        console.log('Added new lifepath: ' + name)
-    } else {
-        console.log('Lifepath already exists!')
+    var newLifePath = {
+        name: name,
+        time: time,
+        leads: leads,
+        skills: skills,
+        traits: traits,
     }
+
+    var pathStock = getStock(paths, stock)
+
+    // Step 1: Find the appropriate stock:
+    if (pathStock) {
+        // Step 2: Find the correct setting
+        var pathSetting = getSetting(pathStock, setting)
+
+        if (pathSetting) {
+            var path = getPath(pathSetting, name)
+            // If the lifepath already exists
+            if (path) {
+                return console.log('Lifepath: ' + name + ' from setting: ' + setting + 'from stock: ' + stock + ' already exists!')
+            // Lifepath doesn't exist
+            } else {
+                pathSetting.lifePaths.push(newLifePath)
+                console.log('Added lifepath: ' + name + ' to the ' + stock + ' stock and ' + setting + ' setting.')
+            }
+        // No setting exists
+        } else {
+            pathStock.settings.push( {setting: setting, lifePaths: [newLifePath]} )
+            console.log('Added new setting to stock: ' + stock + ' with lifepath: ' + name)
+        }
+    // No stock exists:
+    } else {
+        var pathArray = [ newLifePath ]
+        var settingArray = [{ setting: setting, lifePaths: pathArray }]
+        var newStock = { stock: stock, settings : settingArray }
+        paths.push(newStock)
+        console.log('Added new stock: ' + stock + ' with setting: ' + setting + ' and lifePath: ' + name)
+    }
+
+    saveLifePath(paths)
+}
+
+const getPath = (setting, pathName) => {
+    return setting.lifePaths.filter((lp) => lp.name.toLowerCase() === pathName.toLowerCase())
+    .find((lp) => lp.name.toLowerCase() === pathName.toLowerCase())
+}
+
+const getSetting = (stock, setting) => {
+    return stock.settings.filter((se) => se.setting.toLowerCase() === setting.toLowerCase())
+    .find((se) => se.setting.toLowerCase() === setting.toLowerCase())
+}
+
+const getStock = (paths, stock) => {
+    return paths.filter((st) => st.stock.toLowerCase() === stock.toLowerCase())
+        .find((st) => st.stock.toLowerCase() === stock.toLowerCase())
 }
 
 const saveLifePath = (paths) => {
